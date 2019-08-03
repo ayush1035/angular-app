bf769620f2534333dae292748e6648d1a7b59086
ayush1035 <jain20131035@gmail.com>
2019-08-03 17:18:34 +0530

    first push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { FormComponent } from './core/form/form.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: FormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/commonComponents/header/header.component';
+import { DialogComponent } from './core/commonComponents/dialog/dialog.component';
+import { FooterComponent } from './core/commonComponents/footer/footer.component';
+import { DialogService } from './core/commonComponents/dialog/dialog.service';
+import { FormComponent } from './core/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DialogComponent,
+    FooterComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  entryComponents: [DialogComponent],
+
+  providers: [DialogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
